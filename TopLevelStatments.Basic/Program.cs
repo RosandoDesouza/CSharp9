35e1ba6e85752d353a7ee67f87ad7402b8bd62e2
Rosando Desouza <desouza.rosando@gmail.com>
2020-11-18 22:38:45 +0530

    Create TopLevelStatments.Basic project

---
+++ TopLevelStatments.Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TopLevelStatments.Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
