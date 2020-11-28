cc5fe7990c29df7a7e8272b9466a90f64783ba81
RosandoDesouza <desouza.rosando@gmail.com>
2020-11-29 00:38:30 +0530

    Add object initialization with target typed new expressions

--- TargetTypedNewExpressions/Program.cs
+++ TargetTypedNewExpressions/Program.cs
@@ -3,3 +3,15 @@ using System.Collections.Generic;
 
 Console.WriteLine("Beginning of code");
 
+Dictionary<string, string> hash = new()
+{
+    { "a", "asdf" }
+};
+
+Person p = new() 
+{
+    FirstName = "RDE",
+    LastName = "Code"
+};
+
+p.DisplayFullName();
