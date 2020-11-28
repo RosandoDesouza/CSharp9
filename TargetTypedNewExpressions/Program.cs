b9a25fb43487ded91c68740d363cd485ea89fffa
RosandoDesouza <desouza.rosando@gmail.com>
2020-11-29 00:24:51 +0530

    Revered program class of target typed new expressions project

--- TargetTypedNewExpressions/Program.cs
+++ TargetTypedNewExpressions/Program.cs
@@ -3,15 +3,3 @@ using System.Collections.Generic;
 
 Console.WriteLine("Beginning of code");
 
-Dictionary<string, string> hash = new()
-{
-    { "a", "asf" }
-};
-
-Person person = new()
-{ 
-    FirstName = "RDE", 
-    LastName = "Code"
-};
-
-person.DisplayFullName();
