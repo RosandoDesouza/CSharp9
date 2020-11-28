cf07a07e2ba17629995d6855744352d8a7abaf2f
RosandoDesouza <desouza.rosando@gmail.com>
2020-11-29 00:22:28 +0530

    Added project for targer typed new expressions

---
+++ TargetTypedNewExpressions/Person.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Person 
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public void DisplayFullName() 
+    {
+        Console.WriteLine($"{FirstName} {LastName}");
+    }
+}
---
+++ TargetTypedNewExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Beginning of code");
+
+Dictionary<string, string> hash = new()
+{
+    { "a", "asf" }
+};
+
+Person person = new()
+{ 
+    FirstName = "RDE", 
+    LastName = "Code"
+};
+
+person.DisplayFullName();
